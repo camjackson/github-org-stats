5ae0c3ee7c1b166be3873c4469c4ec0b47f2207c
Cam Jackson <camjackson89@gmail.com>
2017-03-08 22:35:10 +1100

    Initial working version of contribution count across an org

--- src/App.js
+++ src/App.js
@@ -3,6 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { contributors: [] }
+  }
+
+  componentDidMount() {
+    const organisation = '<INSERT ORG NAME HERE>';
+    const headers = new Headers();
+    headers.append('Authorization', 'token <INSERT OAUTH TOKEN HERE>')
+
+    const contributors = {};
+    const addContributions = (user, contributions) => {
+      contributors[user] = (contributors[user] || 0) + contributions;
+    };
+    fetch(`https://api.github.com/orgs/${organisation}`, { headers })
+      .then(res => res.json())
+      .then(org => fetch(org.repos_url, { headers }))
+      .then(res => res.json())
+      .then(repos => Promise.all(repos.map(repo => fetch(repo.contributors_url, { headers }))))
+      .then(resArr => Promise.all(resArr.map(res => res.json())))
+      .then(repoContributors => {
+        repoContributors.forEach(repo => {
+          repo.forEach(contributor => {
+            addContributions(contributor.login, contributor.contributions);
+          });
+        });
+        const contributorArray = Object.keys(contributors).map(name => ({ name, contributions: contributors[name] }));
+        const sortedContributors = contributorArray.sort((left, right) => right.contributions - left.contributions);
+
+        this.setState({ contributors: sortedContributors });
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +43,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table style={{ margin: '0 auto' }}>
+          <thead><tr><th>User</th><th>Contributions</th></tr></thead>
+          <tbody>
+            {this.state.contributors.map(contributor => (
+              <tr key={contributor.name}>
+                <td>{contributor.name}</td>
+                <td>{contributor.contributions}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
